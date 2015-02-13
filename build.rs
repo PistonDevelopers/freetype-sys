fde096a644eabd8f69c107a75c399710f0e7c101
Potpourri <pot_pourri@mail.ru>
2015-02-14 01:13:13 +0300

    Add feature nopkgcfg

--- build.rs
+++ build.rs
@@ -1,14 +1,16 @@
+#![feature(env)]
+
 extern crate "pkg-config" as pkg_config;
 
+use std::env::var;
+use pkg_config::find_library;
+
 fn main() {
-    match pkg_config::find_library("freetype2") {
-        Ok(_) => return,
-        Err(_) => {
-            if cfg!(windows) {
-                println!("cargo:rustc-flags=-l freetype-6:dylib");
-            } else {
-                println!("cargo:rustc-flags=-l freetype:dylib");
-            }
+    if var("CARGO_FEATURE_NOPKGCFG").is_ok() || find_library("freetype2").is_err() {
+        if cfg!(windows) {
+            println!("cargo:rustc-flags=-l freetype-6:dylib");
+        } else {
+            println!("cargo:rustc-flags=-l freetype:dylib");
         }
     }
 }
