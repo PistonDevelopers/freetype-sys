6fabcc44ad47047d8263fbf18c070cdca389572c
Birunthan Mohanathas <birunthan@mohanathas.com>
2019-05-24 16:45:11 +0300

    Allow opting out of Freetype system library linkage
    In our project, we compile Freetype into the Rust binary ourselves and
    don't want to link to the system library. This adds the `link-freetype`
    option (enabled by default) to ensure that the system library is never
    used.

--- build.rs
+++ build.rs
@@ -1,6 +1,8 @@
+#[cfg(feature = "link-freetype")]
 extern crate pkg_config;
 
 fn main() {
+    #[cfg(feature = "link-freetype")]
     match pkg_config::find_library("freetype2") {
         Ok(_) => return,
         Err(_) => {
