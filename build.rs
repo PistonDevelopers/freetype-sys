afe058bd95a72cee56d1232ae06d6e03a1da1950
Christian Duerr <contact@christianduerr.com>
2020-02-24 17:34:31 +0100

    Build freetype from source on android
    This adapts the build process of servo-freetype-sys to build freetype2
    from source when on the android target.
    
    Since this is the only difference preventing servo from switching to
    freetype-sys, it would then be possible to deprecate the
    servo-freetype-sys package in favor of having only a single library
    linking to the freetype project.
    
    Without unifying the different freetype bindings, it is not possible for
    the Alacritty project to build without having to manually replace one of
    the depedencies using the patch section in the Cargo.toml, which is not
    only ugly but also prevents pushing to crates.io. Of course it would be
    possible to completely switch to servo-freetype, but I think it would be
    beneficial for the entire Rust ecosystem to be able to freely combine
    the different libraries.
    
    This was originally discussed with servo in
    https://github.com/servo/libfontconfig/pull/46, where they've offered to
    cooperate on this effort. While they've stated that they're willing to
    give up control over freetype-sys completely, it might be nice to share
    control over the repository and crates.io.

--- build.rs
+++ build.rs
@@ -1,10 +1,31 @@
+extern crate cmake;
 extern crate pkg_config;
 
+use cmake::Config;
+use std::env;
+
 fn main() {
-    match pkg_config::find_library("freetype2") {
-        Ok(_) => return,
-        Err(_) => {
-            println!("cargo:rustc-link-lib=dylib=freetype");
-        }
+    let target = env::var("TARGET").unwrap();
+    if !target.contains("eabi")
+        && !target.contains("android")
+        && pkg_config::Config::new().atleast_version("18.5.12").find("freetype2").is_ok()
+    {
+        return
     }
+
+    let mut config = Config::new("freetype2");
+    if let Ok(s) = env::var("FREETYPE_CMAKE_GENERATOR") {
+        config.generator(s);
+    }
+    let dst = config
+        .define("WITH_BZip2", "OFF")
+        .define("WITH_HarfBuzz", "OFF")
+        .define("WITH_PNG", "OFF")
+        .define("WITH_ZLIB", "OFF")
+        .profile("Release")
+        .build();
+    let out_dir = env::var("OUT_DIR").unwrap();
+    println!("cargo:rustc-link-search=native={}/lib", dst.display());
+    println!("cargo:rustc-link-lib=static=freetype");
+    println!("cargo:outdir={}", out_dir);
 }
